c353a8d2f419efac3d1052d97f88b1118b485783
nat corpuz <nate.corpuz1@gmail.com>
2022-06-01 10:03:22 +0800

    reviews auto scrolling, wip cta style

--- src/components/Home/HomeReviews.js
+++ src/components/Home/HomeReviews.js
@@ -1,117 +1,223 @@
-import { Row, Col, Card } from "react-bootstrap";
+import { Row, Col, Card, Container, Button } from "react-bootstrap";
 import "./HomeReviews.css";
 
 const HomeReviews = () => {
 	return (
-		<>
-			<h1 className="text-header text-prime py-5 text-center">
+		<div className="d-flex flex-column">
+			<h1 className="text-header text-prime pt-5 text-center">
 				What our customers say
 			</h1>
-			<Row className="home-reviews-row">
-				<Col sm={5} md={4} lg={3}>
-					<Card>
-						<Card.Img variant="top" src="https://via.placeholder.com/600x300" />
-						<Card.Body>
-							<blockquote className="blockquote mb-0">
-								<p className="text-content">
-									{" "}
-									Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-									Integer posuere erat a ante.{" "}
-								</p>
-								<footer className="blockquote-footer text-content">
-									John Smith, from <cite title="Source Title">Pasig</cite>
-								</footer>
-							</blockquote>
-						</Card.Body>
-					</Card>
-				</Col>
-				<Col sm={5} md={4} lg={3}>
-					<Card>
-						<Card.Img variant="top" src="https://via.placeholder.com/600x300" />
-						<Card.Body>
-							<blockquote className="blockquote mb-0">
-								<p className="text-content">
-									{" "}
-									Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-									Integer posuere erat a ante.{" "}
-								</p>
-								<footer className="blockquote-footer text-content">
-									John Smith, from <cite title="Source Title">Pasig</cite>
-								</footer>
-							</blockquote>
-						</Card.Body>
-					</Card>
-				</Col>
-				<Col sm={5} md={4} lg={3}>
-					<Card>
-						<Card.Img variant="top" src="https://via.placeholder.com/600x300" />
-						<Card.Body>
-							<blockquote className="blockquote mb-0">
-								<p className="text-content">
-									{" "}
-									Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-									Integer posuere erat a ante.{" "}
-								</p>
-								<footer className="blockquote-footer text-content">
-									John Smith, from <cite title="Source Title">Pasig</cite>
-								</footer>
-							</blockquote>
-						</Card.Body>
-					</Card>
-				</Col>
-				<Col sm={5} md={4} lg={3}>
-					<Card>
-						<Card.Img variant="top" src="https://via.placeholder.com/600x300" />
-						<Card.Body>
-							<blockquote className="blockquote mb-0">
-								<p className="text-content">
-									{" "}
-									Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-									Integer posuere erat a ante.{" "}
-								</p>
-								<footer className="blockquote-footer text-content">
-									John Smith, from <cite title="Source Title">Pasig</cite>
-								</footer>
-							</blockquote>
-						</Card.Body>
-					</Card>
-				</Col>
-				<Col sm={5} md={4} lg={3}>
-					<Card>
-						<Card.Img variant="top" src="https://via.placeholder.com/600x300" />
-						<Card.Body>
-							<blockquote className="blockquote mb-0">
-								<p className="text-content">
-									{" "}
-									Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-									Integer posuere erat a ante.{" "}
-								</p>
-								<footer className="blockquote-footer text-content">
-									John Smith, from <cite title="Source Title">Pasig</cite>
-								</footer>
-							</blockquote>
-						</Card.Body>
-					</Card>
-				</Col>
-				<Col sm={5} md={4} lg={3}>
-					<Card>
-						<Card.Img variant="top" src="https://via.placeholder.com/600x300" />
-						<Card.Body>
-							<blockquote className="blockquote mb-0">
-								<p className="text-content">
-									{" "}
-									Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-									Integer posuere erat a ante.{" "}
-								</p>
-								<footer className="blockquote-footer text-content">
-									John Smith, from <cite title="Source Title">Pasig</cite>
-								</footer>
-							</blockquote>
-						</Card.Body>
-					</Card>
-				</Col>
-			</Row>
-		</>
+			<p className="text-center mx-auto">
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi
+				voluptate similique a magni hic ab adipisci sed incidunt voluptatum
+				suscipit!
+			</p>
+			<Container className="p-0 m-0 home-reviews-container mx-auto">
+				<Row className="d-flex flex-nowrap home-reviews">
+					<Col sm={5} md={4} lg={3} className="p-0">
+						<Card>
+							<Card.Img
+								variant="top"
+								src="https://via.placeholder.com/600x300"
+							/>
+							<Card.Body>
+								<blockquote className="blockquote mb-0">
+									<p className="text-content">
+										{" "}
+										Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+										Integer posuere erat a ante.{" "}
+									</p>
+									<footer className="blockquote-footer text-content">
+										John Smith, from <cite title="Source Title">Pasig</cite>
+									</footer>
+								</blockquote>
+							</Card.Body>
+						</Card>
+					</Col>
+					<Col sm={5} md={4} lg={3} className="p-0">
+						<Card>
+							<Card.Img
+								variant="top"
+								src="https://via.placeholder.com/600x300"
+							/>
+							<Card.Body>
+								<blockquote className="blockquote mb-0">
+									<p className="text-content">
+										{" "}
+										Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+										Integer posuere erat a ante.{" "}
+									</p>
+									<footer className="blockquote-footer text-content">
+										John Smith, from <cite title="Source Title">Pasig</cite>
+									</footer>
+								</blockquote>
+							</Card.Body>
+						</Card>
+					</Col>
+					<Col sm={5} md={4} lg={3} className="p-0">
+						<Card>
+							<Card.Img
+								variant="top"
+								src="https://via.placeholder.com/600x300"
+							/>
+							<Card.Body>
+								<blockquote className="blockquote mb-0">
+									<p className="text-content">
+										{" "}
+										Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+										Integer posuere erat a ante.{" "}
+									</p>
+									<footer className="blockquote-footer text-content">
+										John Smith, from <cite title="Source Title">Pasig</cite>
+									</footer>
+								</blockquote>
+							</Card.Body>
+						</Card>
+					</Col>
+					<Col sm={5} md={4} lg={3} className="p-0">
+						<Card>
+							<Card.Img
+								variant="top"
+								src="https://via.placeholder.com/600x300"
+							/>
+							<Card.Body>
+								<blockquote className="blockquote mb-0">
+									<p className="text-content">
+										{" "}
+										Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+										Integer posuere erat a ante.{" "}
+									</p>
+									<footer className="blockquote-footer text-content">
+										John Smith, from <cite title="Source Title">Pasig</cite>
+									</footer>
+								</blockquote>
+							</Card.Body>
+						</Card>
+					</Col>
+					<Col sm={5} md={4} lg={3} className="p-0">
+						<Card>
+							<Card.Img
+								variant="top"
+								src="https://via.placeholder.com/600x300"
+							/>
+							<Card.Body>
+								<blockquote className="blockquote mb-0">
+									<p className="text-content">
+										{" "}
+										Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+										Integer posuere erat a ante.{" "}
+									</p>
+									<footer className="blockquote-footer text-content">
+										John Smith, from <cite title="Source Title">Pasig</cite>
+									</footer>
+								</blockquote>
+							</Card.Body>
+						</Card>
+					</Col>
+					<Col sm={5} md={4} lg={3} className="p-0">
+						<Card>
+							<Card.Img
+								variant="top"
+								src="https://via.placeholder.com/600x300"
+							/>
+							<Card.Body>
+								<blockquote className="blockquote mb-0">
+									<p className="text-content">
+										{" "}
+										Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+										Integer posuere erat a ante.{" "}
+									</p>
+									<footer className="blockquote-footer text-content">
+										John Smith, from <cite title="Source Title">Pasig</cite>
+									</footer>
+								</blockquote>
+							</Card.Body>
+						</Card>
+					</Col>
+					<Col sm={5} md={4} lg={3} className="p-0">
+						<Card>
+							<Card.Img
+								variant="top"
+								src="https://via.placeholder.com/600x300"
+							/>
+							<Card.Body>
+								<blockquote className="blockquote mb-0">
+									<p className="text-content">
+										{" "}
+										Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+										Integer posuere erat a ante.{" "}
+									</p>
+									<footer className="blockquote-footer text-content">
+										John Smith, from <cite title="Source Title">Pasig</cite>
+									</footer>
+								</blockquote>
+							</Card.Body>
+						</Card>
+					</Col>
+					<Col sm={5} md={4} lg={3} className="p-0">
+						<Card>
+							<Card.Img
+								variant="top"
+								src="https://via.placeholder.com/600x300"
+							/>
+							<Card.Body>
+								<blockquote className="blockquote mb-0">
+									<p className="text-content">
+										{" "}
+										Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+										Integer posuere erat a ante.{" "}
+									</p>
+									<footer className="blockquote-footer text-content">
+										John Smith, from <cite title="Source Title">Pasig</cite>
+									</footer>
+								</blockquote>
+							</Card.Body>
+						</Card>
+					</Col>
+					<Col sm={5} md={4} lg={3} className="p-0">
+						<Card>
+							<Card.Img
+								variant="top"
+								src="https://via.placeholder.com/600x300"
+							/>
+							<Card.Body>
+								<blockquote className="blockquote mb-0">
+									<p className="text-content">
+										{" "}
+										Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+										Integer posuere erat a ante.{" "}
+									</p>
+									<footer className="blockquote-footer text-content">
+										John Smith, from <cite title="Source Title">Pasig</cite>
+									</footer>
+								</blockquote>
+							</Card.Body>
+						</Card>
+					</Col>
+					<Col sm={5} md={4} lg={3} className="p-0">
+						<Card>
+							<Card.Img
+								variant="top"
+								src="https://via.placeholder.com/600x300"
+							/>
+							<Card.Body>
+								<blockquote className="blockquote mb-0">
+									<p className="text-content">
+										{" "}
+										123Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+										Integer posuere erat a ante.{" "}
+									</p>
+									<footer className="blockquote-footer text-content">
+										John Smith, from <cite title="Source Title">Pasig</cite>
+									</footer>
+								</blockquote>
+							</Card.Body>
+						</Card>
+					</Col>
+				</Row>
+			</Container>
+			<Button className="custom-btn-2 mx-auto">See all reviews</Button>
+		</div>
 	);
 };
 
