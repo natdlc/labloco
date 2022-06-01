e19d72fe3d4704a67202ddb2b7e868112ca2ca03
nat corpuz <nate.corpuz1@gmail.com>
2022-06-01 15:41:32 +0800

    featured bundles

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import AppNav from "./components/AppNav";
+import AppNav from "./components/AppNav/AppNav";
 import Home from "./pages/Home";
 
 function App() {
--- src/components/Home/FeaturedBundles/FeaturedBundles.jsx
+++ src/components/Home/FeaturedBundles/FeaturedBundles.jsx
@@ -4,42 +4,31 @@ import imgPlaceholder from "../../../assets/imgs/featured-bundles-img.png";
 
 const FeaturedBundles = () => {
 	return (
-		<>
+		<div className="d-flex flex-column">
 			<h1 className="text-header text-prime pt-5 text-center">
 				Featured Bundles
 			</h1>
 			<p className="text-center mx-auto subheader-custom">
-				Save more by buying in bulk
+				<span className="text-prime fw-bold">Save more</span> by buying in bulk
 			</p>
 			<Container>
-				<Row className="gap-5">
-					<Col>
-						<Card className="custom-shade-bg-1 mx-auto" style={{ width: "18rem" }}>
+				<Row className="gap-5 justify-content-center">
+					<Col lg={5} xl={4}>
+						<Card className="custom-shade-bg-1">
 							<Card.Img className="bg-white p-3" src={imgPlaceholder} />
 							<Button className="custom-btn-2 py-3 fs-4">BASIC</Button>
 						</Card>
 					</Col>
-					<Col>
-						<Card className="custom-shade-bg-1 mx-auto" style={{ width: "18rem" }}>
-							<Card.Img className="bg-white p-3" src={imgPlaceholder} />
-							<Button className="custom-btn-2 py-3 fs-4">STANDARD</Button>
-						</Card>
-					</Col>
-					<Col>
-						<Card className="custom-shade-bg-1 mx-auto" style={{ width: "18rem" }}>
-							<Card.Img className="bg-white p-3" src={imgPlaceholder} />
-							<Button className="custom-btn-2 py-3 fs-4">PRO</Button>
-						</Card>
-					</Col>
-					<Col>
-						<Card className="custom-shade-bg-1 mx-auto" style={{ width: "18rem" }}>
+					<Col lg={5} xl={4}>
+						<Card className="custom-shade-bg-1">
 							<Card.Img className="bg-white p-3" src={imgPlaceholder} />
 							<Button className="custom-btn-2 py-3 fs-4">BARKADA</Button>
 						</Card>
 					</Col>
 				</Row>
 			</Container>
-		</>
+			<Button className="custom-btn-2 mx-auto mt-4">See all bundles</Button>
+		</div>
 	);
 };
 
